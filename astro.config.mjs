9e1cdde55e21ccb5ef06a2e0f53420ffa738d7f0
wangjiangyu-creator <wangjiangyu@gmail.com>
2026-05-23 19:05:03 +0800

    Build site for custom domain root

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://wangjiangyu-creator.github.io",
-  base: "/China-international-law",
+  site: "https://int.eastlaw.wang",
   integrations: [mdx()],
   output: "static",
 });
